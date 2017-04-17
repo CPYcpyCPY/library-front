d3adcd9ef5fe6e84a88256488b4b76ab75207a3c
陈平永 <734162700@qq.com>
2017-04-17 12:51:33 +0800

    图书馆网站前端

---
+++ src/components/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="admin">
+    管理员
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Admin'
+  }
+</script>
+
+<style scoped>
+  #admin {
+    background-color: aquamarine;
+  }
+</style>
